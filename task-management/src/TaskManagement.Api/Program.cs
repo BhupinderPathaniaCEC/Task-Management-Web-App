a77ecc3e23f67eebb703295fc273d383eae83062
Bhupinder <2220880.cse.coe@cgc.edu.in>
2026-05-03 18:36:57 +0530

    Add Railway PORT environment variable support

--- task-management/src/TaskManagement.Api/Program.cs
+++ task-management/src/TaskManagement.Api/Program.cs
@@ -68,4 +68,5 @@ using (var scope = app.Services.CreateScope())
 
 app.MapControllers();
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
